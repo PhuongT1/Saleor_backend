d682403a912282df84633e23a71b547e011b9df1
BM Loukas <loukas@brickmate.vn>
2024-07-22 17:33:03 +0700

    feat (Zone) :

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,9 @@
 # gunicorn.conf.py
 import os
 
-workers = 3
+workers = 4
 worker_class = 'uvicorn.workers.UvicornWorker'
 host = '0.0.0.0'
 port = os.environ.get("PORT",8000)
 loglevel = 'info'
-timeout=500
+timeout=30

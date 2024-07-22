b86d38b19227e65cc5424260820c2dfb9530863d
BM Loukas <loukas@brickmate.vn>
2024-07-22 17:20:58 +0700

    feat (Gunicorn) : Update gunicorn file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,9 @@
 # gunicorn.conf.py
 import os
 
-workers = 1
+workers = 3
 worker_class = 'uvicorn.workers.UvicornWorker'
 host = '0.0.0.0'
 port = os.environ.get("PORT",8000)
 loglevel = 'info'
-timeout=1000
+timeout=500
